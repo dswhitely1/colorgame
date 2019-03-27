2fbf7206c5d2b08592f56f98925216464dffc60f
[Donald Whitely] <[dswhitely1@gmail.com]>
2019-03-27 18:45:27 -0400

    Trying to update Master
    Here we go

--- src/components/SquareDisplay.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SquareDisplay = (props) => {
-	return <div id='container'>{props.children()}</div>;
-};
-
-export default SquareDisplay;
--- src/components/Squares.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Squares = (props) => {
-	let divStyle = { backgroundColor: props.square };
-	return <div onClick={props.squareChoice} className='square' style={divStyle} />;
-};
-
-export default Squares;
